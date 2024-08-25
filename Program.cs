a5967f95a3abef2b53dc7a94948fd345412d4d40
Kunal Sharma <kk.s67533@gmail.com>
2024-08-25 21:05:27 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(" World!!!");
+// // Console.WriteLine("Praise the great leader... Kunal Sharma");
+// string KunalSharma = "My name is Kunal Sharma";
+// Console.WriteLine(KunalSharma);
+
+
+
+//crud 
+//Create Read Update Delete.
+
+//Create and Concatinate
+var name = "Kunal";
+Console.WriteLine(name);
+var lname = "Sharma ";
+var fullname = name + lname;
+Console.WriteLine(fullname);
+
+//Templates Literals 
+Console.WriteLine($"I am buying a puffer fish {fullname} you cannot stop me");
+
+//Read
+Console.WriteLine(fullname);
+
+//Update 
+string newfish = fullname.Replace("KunalSharma","Gaurav Myana");
+Console.WriteLine(newfish);
